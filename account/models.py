c2e853671dd205a38d9c9deb990179d4c491a3de
samkayz <ilemobayosamson@gmail.com>
2019-10-03 08:10:19 +0100

    Validation

--- account/models.py
+++ account/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class Account(models.Model):
     username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     phone_no = models.CharField(max_length=20)
     customer_id = models.CharField(max_length=10)
     bal = models.CharField(max_length=200)
+
+
+class Transactions(models.Model):
+    sender = models.CharField(max_length=50)
+    receiver = models.CharField(max_length=50)
+    amount = models.CharField(max_length=200)
+    ref_no = models.CharField(max_length=40)
+    date = models.DateTimeField(default=datetime.now())
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-from .models import Account
+from .models import Account, Transactions
 import random
 import string
 import uuid
@@ -52,6 +52,8 @@ def register(request):
                                                 last_name=last_name,
                                                 )
                 users = Account(username=username,
+                                first_name=first_name,
+                                last_name=last_name,
                                 phone_no=phone_no,
                                 customer_id=cus_id,
                                 bal='0',)
@@ -80,15 +82,48 @@ def logout(request):
 
 
 def transfer(request):
-    c_user = request.user.username
-    show = Account.objects.all().get(username=c_user)
-    context = {'show': show}
-    return render(request, 'transfer.html', context)
+    ref_no = uuid.uuid4().hex[:10].upper()
+    if request.method == 'POST':
+        s_username = request.POST['s_username']
+        r_number = request.POST['r_number']
+        amount = request.POST['amount']
+        bal = Account.objects.values('bal').get(phone_no=r_number)['bal']
+        s_bal = Account.objects.values('bal').get(username=s_username)['bal']
+        show = Account.objects.values().get(phone_no=r_number)['username']
+        print("Hello: ", show)
+        sb = (float(s_bal))
+        rb = (float(bal))
+        am = (float(amount))
+
+        if am > sb:
+            messages.info(request, 'Insufficient Balance')
+        elif s_username == show:
+            messages.info(request, "You Can't Send Money to Yourself!!")
+        else:
+            new = rb + am
+            Account.objects.filter(phone_no=r_number).update(bal=new)
+
+            new_2 = sb - am
+            Account.objects.filter(username=s_username).update(bal=new_2)
+
+            trans = Transactions(sender=s_username,
+                                 receiver=r_number,
+                                 amount=amount,
+                                 ref_no=ref_no,)
+            trans.save()
+            messages.info(request, "Transaction Successful!!")
+    return render(request, 'transfer.html')
 
 
 def verify(request):
     if request.method == 'POST':
         r_number = request.POST['number']
-        check = Account.objects.all().get(phone_no=r_number)
-        context = {'check': check}
-    return render(request, 'transfer.html', context)
+        if Account.objects.filter(phone_no=r_number).exists():
+            check = Account.objects.all().get(phone_no=r_number)
+            cont = {'check': check}
+            return render(request, 'transfer.html', cont)
+        else:
+            messages.info(request, 'Mobile Number Not Found')
+            return redirect('transfer')
+
+    return render(request, 'transfer.html')
